d487be7f3acce5fa8221d520fe29ef5e0167ac6a
Connie Leung <cleung334@gmail.com>
2019-04-02 23:01:13 +0800

    chore: fix dependencies
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/lazy/lazy.module.ts
+++ src/app/lazy/lazy.module.ts
@@ -1,18 +1,14 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { PriceModule } from "../price";
-import { ShareModule } from "../share";
-import { LazyComponent } from "./lazy-comp/lazy.component";
-import { LazyCreateDirectiveComponent } from "./lazy-creation-directive/lazy-creation-directive.component";
-import { LazyCreationComponent } from "./lazy-creation/lazy-creation.component";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { PriceModule } from '../price';
+import { SharedModule } from '../shared';
+import { LazyComponent } from './lazy-comp/lazy.component';
+import { LazyCreateDirectiveComponent } from './lazy-creation-directive/lazy-creation-directive.component';
+import { LazyCreationComponent } from './lazy-creation/lazy-creation.component';
 
 @NgModule({
-  imports: [CommonModule, PriceModule, ShareModule],
-  declarations: [
-    LazyComponent,
-    LazyCreationComponent,
-    LazyCreateDirectiveComponent
-  ],
-  exports: [LazyComponent, LazyCreationComponent, LazyCreateDirectiveComponent]
+    imports: [CommonModule, PriceModule, SharedModule],
+    declarations: [LazyComponent, LazyCreationComponent, LazyCreateDirectiveComponent],
+    exports: [LazyComponent, LazyCreationComponent, LazyCreateDirectiveComponent],
 })
 export class LazyModule {}
