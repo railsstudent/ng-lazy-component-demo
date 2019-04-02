1ce356d04d2d6b9b591dde2ce4e7d0d0449b3ae3
Connie Leung <cleung334@gmail.com>
2019-04-02 22:40:15 +0800

    chore: rename to SharedModule
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,16 @@
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { BrowserModule } from "@angular/platform-browser";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { LazyModule } from "./lazy";
-import { NavListComponent } from "./nav-list/nav-list.component";
-import { PriceModule } from "./price";
-import { ShareModule } from "./share";
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LazyModule } from './lazy';
+import { NavListComponent } from './nav-list/nav-list.component';
+import { PriceModule } from './price';
+import { SharedModule } from './shared';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    PriceModule,
-    AppRoutingModule,
-    LazyModule,
-    ShareModule
-  ],
-  declarations: [AppComponent, NavListComponent],
-  bootstrap: [AppComponent]
+    imports: [BrowserModule, FormsModule, AppRoutingModule, PriceModule, AppRoutingModule, LazyModule, SharedModule],
+    declarations: [AppComponent, NavListComponent],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
